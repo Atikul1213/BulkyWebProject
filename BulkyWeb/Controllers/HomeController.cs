f58a3adf4eadcf39d59276fcfd718ca06137f201
Atikul1213 <atikul.islam@brainstation-23.com>
2024-05-28 12:50:44 +0600

    Add Details page

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -24,6 +24,13 @@ namespace Bulky.Controllers
         }
     
 
+
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == id, includeProperties: "Category");
+            return View(product);
+
+        }
         public IActionResult Privacy()
         {
             return View();
